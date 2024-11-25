effa39b666677c60e8cdff742caa87e5f6481a16
gajin2 <gajin2@outlook.com>
2024-11-25 21:35:23 +0800

    del ts-out-dir

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,19 +3,10 @@
 </template>
 
 <script setup>
-import LoginAlt from './login-alt.vue'
 import LoginBase from './login-basic.vue'
-import LoginLighting from './login-lighting.vue'
-
-const renderNum = Math.floor(Math.random() * 10) + 1
 
 const Component = computed(() => {
-  if (renderNum <= 2) {
-    return LoginBase
-  } else if (renderNum <= 4 && renderNum > 2) {
-    return LoginLighting
-  }
-  return LoginAlt
+    return LoginBase;
 })
 </script>
 
